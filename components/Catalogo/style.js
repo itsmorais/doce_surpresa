db5b0c931e70691a5cdc630351538ad664472d0e
Michael Morais <michael_morais@Outlook.com.br>
2023-05-05 15:58:08 -0300

    mobile phone official

--- components/Catalogo/style.js
+++ components/Catalogo/style.js
@@ -11,6 +11,7 @@ export const CatalogoButton = styled.button`
     background: linear-gradient(180deg, #0D1321 0%, rgba(13, 19, 33, 0.8) 53.12%, rgba(13, 19, 33, 0.7) 83.33%);
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
+    border: transparent;
 
     >a{
     text-decoration: none;
