3b1dee33d3312eddbde00aa63f53d3a226cd60b3
Michael Morais <michael_morais@Outlook.com.br>
2023-05-08 23:22:09 -0300

    dev build fix remote

--- pages/catalogo/[productType].js
+++ pages/catalogo/[productType].js
@@ -31,23 +31,9 @@ export default function Catalogo({data}) {
   )
 }
 
-export async function getStaticPaths() {
+
+export async function getServerSideProps({ params }) {
   const cestas = await fetch("https://www.docesurpresacestaria.com.br/api/hello")
-  const data = await cestas.json()
-
-  // Generate paths for all product types
-  const paths = Object.keys(data.cestas).map((productType) => ({
-    params: { productType },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const cestas = await fetch("http://www.localhost:3000/api/hello")
   const data = await cestas.json()
 
   const productType = params.productType;
@@ -55,6 +41,6 @@ export async function getStaticProps({ params }) {
   const cesta = data.cestas[productType];
 
   return {
-    props: { data, nome, cesta },
+    props: { cesta},
   };
 }
