f753203d8115594eb4f037054c76cd80ddd92022
Michael morais <53665466+itsmorais@users.noreply.github.com>
2023-08-01 12:21:55 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'drive.google.com',
+        hostname: 'drive.google.com/uc?export=view&id',
         port: '',
         pathname: '**',
       },
