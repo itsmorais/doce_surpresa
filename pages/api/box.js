02ce099597dafbd311edb68e2e005d39810a4758
Michael Morais <michael_morais@Outlook.com.br>
2023-08-02 15:54:17 -0300

    alterando dados da cesta sem preencher url da foto

--- pages/api/box.js
+++ pages/api/box.js
@@ -42,40 +42,66 @@ export default async function handler(req, res) {
     }
 
   } else if (req.method === "PUT") {
-    try{
-    const { box } = req.query;
-    let { cesta_nome = "", preco = "", image_src = "" } = req.body || {};
-    image_src = image_src.replace("/view?usp=drive_link", "");
-    image_src = image_src.replace("file/d/", "uc?export=view&id=");
-    const id = parseInt(box);
-    console.log(image_src,cesta_nome,id)
+    try {
+      const { box } = req.query;
+      let { cesta_nome = "", preco = "", image_src = "" } = req.body || {};
 
-    
-      await prisma.box.update({
-        where: {
-          id,
-        },
-        data: {
-          cesta_nome,
-          preco: parseFloat(preco),
-          image_src,
-        },
-      });
+      if (!image_src) {
+        try {
+          const id = parseInt(box);
+          await prisma.box.update({
+            where: {
+              id,
+            },
+            data: {
+              cesta_nome,
+              preco: parseFloat(preco),
+            },
+          });
+          return res.status(200).json();
+        } catch (error) {
+          return res.status(400).json({ msg: error.message });
+        }
+      }
 
-      res.status(200).json();
+      try {
+        image_src = image_src.replace("/view?usp=drive_link", "");
+        image_src = image_src.replace("file/d/", "uc?export=view&id=");
+        const id = parseInt(box);
+        console.log(image_src, cesta_nome, id);
+
+        await prisma.box.update({
+          where: {
+            id,
+          },
+          data: {
+            cesta_nome,
+            preco: parseFloat(preco),
+            image_src,
+          },
+        });
+
+        return res.status(200).json();
+
+      } catch (error) {
+        return res.status(400).json({ msg: error.message });
+      }
     } catch (error) {
       res.status(400).json({ msg: error.message });
     }
+
+
+
   } else if (req.method === "DELETE") {
     const { box } = req.query;
-  
+
     try {
       await prisma.box.delete({
         where: {
           id: parseInt(box),
         },
       });
-  
+
       res.status(200).json();
     } catch (error) {
       res.status(400).json({ msg: error.message });
