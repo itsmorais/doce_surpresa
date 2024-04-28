019da73a7c610d4e15ae3d039df308d6de42f696
Michael Morais <michael_morais@Outlook.com.br>
2024-04-28 13:46:50 -0300

    Alterando layout

--- components/BoxCard/index.js
+++ components/BoxCard/index.js
@@ -8,28 +8,35 @@ export default function BoxCard({ cestas }) {
     return (
 
 
-        <Web>
+      <>
             {cestas.map(({ cesta_nome, preco, image_src, item, id }) => (
 
                 <Container id="container" key={id}>
                     <h3>{cesta_nome}</h3>
-                    <div id="Container">
+                    <div id="box-Container">
 
+                        <div>
+                        {preco && <p id="preco">R${preco} <span id="frete"> +Frete</span></p>
+                            }
+                            <Link href={`https://wa.me/5512991134425?text=Olá%20,tenho%20interesse%20no%20box%20${cesta_nome}%20de%20R$%20${preco || ""}`} target="_blank">
+                                <button id="euQuero">Eu Quero</button>
+                            </Link>
+                        </div>
                         <div id="Lista">
                             {item.map((itemUnidade, id) => (
                                 <li key={id}>{itemUnidade.item_nome}</li>
                             ))}
+                        
                         </div>
+                        
+                            <h3>Itens</h3>
+                        
 
                         <div id="Imagem-preco-botao">
                             <div id="ImageContainer">
                                 <Image unoptimized={false} src={`/${image_src}`} width={500} height={300} alt="Imagem da cesta" key={id}></Image>
                             </div>
-                            {preco && <p>R${preco} <span>+Frete</span></p>
-                            }
-                            <Link href={`https://wa.me/5512991134425?text=Olá%20,tenho%20interesse%20no%20box%20${cesta_nome}%20de%20R$%20${preco || ""}`} target="_blank">
-                                <button id="euQuero">Eu Quero</button>
-                            </Link>
+                         
 
 
                         </div>
@@ -37,7 +44,7 @@ export default function BoxCard({ cestas }) {
 
                 </Container>
             ))}
-        </Web>
+        </>
 
     )
 }
--- components/BoxCard/style.js
+++ components/BoxCard/style.js
@@ -2,6 +2,29 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 
+@media (min-width:600px){
+            width: 40vw !important;
+            
+            
+        #ImageContainer{
+                position: relative;
+                width: 20vw !important; 
+                height: 200px;
+                overflow: hidden;
+                transition: transform 0.3s ease-in-out;
+
+                &:hover{
+                    transform: scale(1.2);
+                }
+            }
+
+        :nth-child(n){
+            width: 100%;
+        }
+    }
+
+
+
     width: 98%;
     height: fit-content;
     padding: 5%;
@@ -11,10 +34,12 @@ export const Container = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 
               0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
-    margin: 0 auto 6rem auto;
+    margin: 0 auto 7rem auto;
 
-    >h3{
-    font-family: 'DM Sans';
+
+
+    h3{
+        font-family: var(--font-poppins);
     font-style: normal;
     font-weight: 700;
     font-size: 2.1rem;
@@ -30,19 +55,60 @@ export const Container = styled.div`
     >div{
         width: 100%;
         display: flex;
-        margin-top: 1.3rem;
-        gap: 4rem;
+        flex-direction: column-reverse;
+        margin-top: 1rem;
+        gap: 2rem;
+
+        p#preco{
+            font-family: var(--font-poppins);
+                font-style: normal;
+                font-weight: 700;
+                font-size: 2.1rem;
+                line-height: 2.7rem;
+                text-align: center;
+                margin-top: 0rem;
+
+                color: #FFFFFF;
+            }
+            span#frete{
+                font-weight: 300;
+                font-size: 1.3rem;
+            }
+            button#euQuero{
+                
+                width: 11.5rem;
+                height: 3.0rem;
+                background: #FFFAFD;
+                border-radius: 4px;
+                border: none;
+                margin-top: 0.1rem;
+                cursor: pointer;
+
+
+                font-family: var(--font-poppins);
+                font-style: bold;
+                font-weight: 700;
+                font-size: 14px;
+                text-transform: uppercase;
+                line-height: 18px;
+                color: #000000;
+
+                &:hover{
+                    background-color: #000000;
+                    color: #FFFAFD;
+                }
+            }
         
         >div#Lista{
             display: flex;
             align-items: flex-start;
+            gap: 0.2rem;
 
         >li{
-            font-family: 'DM Sans';
-            font-style: normal;
-            font-weight: 700;
-            font-size: 1.4rem;
-            line-height: 1.8rem;
+            font-family: var(--font-poppins);
+            font-weight: 500;
+            font-size: 1.8rem;
+            line-height: 2rem;
 
             /* background */
 
@@ -55,9 +121,9 @@ export const Container = styled.div`
             align-items: center;
             justify-content: center;
 
-            >div#ImageContainer {
+            div#ImageContainer {
                 position: relative;
-                width: 200px;
+                width: 90vw;
                 height: 200px;
                 overflow: hidden;
                 transition: transform 0.3s ease-in-out;
@@ -74,64 +140,12 @@ export const Container = styled.div`
                     border-radius: 15px;
                 }
             }
-            >p{
-                font-family: 'DM Sans';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 2.1rem;
-                line-height: 2.7rem;
-                text-align: center;
-                margin-top: 1rem;
-
-                color: #FFFFFF;
-            }
-            >span{
-                font-weight: 300;
-                font-size: 1.3rem;
-            }
-            button#euQuero{
-                
-                width: 11.5rem;
-                height: 3.0rem;
-                background: #FFFAFD;
-                border-radius: 4px;
-                border: none;
-                margin-top: 1rem;
-                cursor: pointer;
-
-
-                font-family: 'DM Sans';
-                font-style: bold;
-                font-weight: 700;
-                font-size: 14px;
-                text-transform: uppercase;
-                line-height: 18px;
-                color: #000000;
-
-                &:hover{
-                    background-color: #000000;
-                    color: #FFFAFD;
-                }
-            }
+           
         }
         
-        }
-
-
-`
-
-export const Web = styled.div`
-    @media (min-width:900px){
-        margin: auto;
-        display: grid;
-        grid-template-columns: 500px 500px;
-        gap: 1rem;
-        justify-content: space-evenly;
-    
+        
+      
+}
     
 
-        :nth-child(n){
-            width: 100%;
-        }
-    }
 `
